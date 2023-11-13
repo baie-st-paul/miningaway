3931d3daabab55b8b78d3e0778c79e387e5eabd5
Philippe Vallerand <pvallerand1@gmail.com>
2023-11-13 17:40:11 -0500

    setup

---
+++ src/main/kotlin/com/example/BetterthaumcraftDataGenerator.kt
@@ -0,0 +1,9 @@
+package com.example
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object BetterthaumcraftDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
