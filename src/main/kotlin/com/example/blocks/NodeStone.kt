454940e25de229d92fc792745a530d3bacb953ae
Philippe Vallerand <pvallerand1@gmail.com>
2023-12-22 12:42:15 -0500

    wip that ass

--- src/main/kotlin/com/example/blocks/NodeStone.kt
+++ src/main/kotlin/com/example/blocks/NodeStone.kt
@@ -1,16 +1,19 @@
 package com.example.blocks
 
+import net.minecraft.block.BlockRenderType
 import net.minecraft.block.BlockState
 import net.minecraft.block.BlockWithEntity
 import net.minecraft.block.entity.BlockEntity
 import net.minecraft.util.math.BlockPos
-import net.minecraft.world.BlockView
 
 class NodeStone(settings: Settings) : BlockWithEntity(settings) {
     override fun createBlockEntity(pos: BlockPos?, state: BlockState?): BlockEntity {
         return NodeStoneEntity(pos, state)
     }
 
+    override fun getRenderType(state: BlockState?): BlockRenderType {
+        return BlockRenderType.MODEL
+    }
 }
 
 class NodeStoneEntity(pos: BlockPos?, state: BlockState?) : BlockEntity(ModBlocks.NODE_STONE_ENTITY, pos, state) {
